aa155ffd0983b10dba99bf346deca8d2ab5c22d0
ezociaty083 <witayaoc@gmail.com>
2020-09-14 15:42:03 +0700

    Hellow World

---
+++ android/app/src/main/kotlin/com/wittaya/IoTSmartParking/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.wittaya.IoTSmartParking
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Hello WOrld'),
+    );
+  }
+}
